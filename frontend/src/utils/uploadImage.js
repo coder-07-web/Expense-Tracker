805076d40cdc25e02cf4ee19ccc7f39398ba2930
Biswajit <swainbiswajit919@gmail.com>
2026-03-31 17:19:10 +0530

    fixed axiosInstance spelling everywhere

--- frontend/src/utils/uploadImage.js
+++ frontend/src/utils/uploadImage.js
@@ -1,5 +1,5 @@
 import {API_PATHS} from './apiPaths';
-import axiosinstance from './axiosinstance';
+import axiosInstance from './axiosInstance';
 
 const uploadImage = async(imageFile) =>{
     const formData = new FormData();
@@ -7,7 +7,7 @@ const uploadImage = async(imageFile) =>{
     formData.append('image',imageFile);
 
     try{
-        const response = await axiosinstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE,formData,{
+        const response = await axiosInstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE,formData,{
             headers:{
                 'Content-Type':'multipart/form-data', //Set header for file upload
             },
